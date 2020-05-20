52e8256822c269025d46a51b10d386db355174de
翟宇 <zhaiyu@qianxin.com>
2020-05-20 12:00:58 +0800

    :sparkles: feat(master): 完成44
    Signed-off-by: 翟宇 <zhaiyu@qianxin.com>

--- 0044.cpp
+++ 0044.cpp
@@ -1,79 +1,61 @@
 
 #include "data_define.h"
 
-class Solution
-{
+// 最快的 - 回溯牛逼
+class Solution {
 public:
-    bool isMatchHelper(string &s, string &p, int n_s_start, int n_p_start)
-    {
-        bool bRet = true;
-        int nSLen = s.size();
-        int nPLen = p.size();
-        int i = n_s_start, j = n_p_start;
-        while (i < nSLen && j < nPLen) {
-            if (p.at(j) == '*') {
-                if (j == nPLen - 1) {
-                    i = nSLen;
-                    j = nPLen;
-                    break;
-                } else {
-                    if (i == nSLen - 1) {
-                        if (p.at(j + 1) == '?' || p.at(j + 1) == s.at(i)) {
-                            i = nSLen;
-                            j = nPLen;
-                            break;
-                        } else {
-                            i = 0;
-                            j = 0;
-                            break;
-                        }
-                    } else {
-                        if (p.at(j + 1) == s.at(i)) {
-                            bool bTmp = isMatchHelper(s, p, i + 1, j);
-                            if (bTmp) {
-                                i = nSLen;
-                                j = nPLen;
-                                break;
-                            }
-                            ++i;
-                            j += 2;
-                        } else {
-                            ++i;
-                        }
-                    }
-                }
-            } else if (p.at(j) == '?') {
-                ++i, ++j;
-                continue;
-            } else {
-                if (s.at(i) == p.at(j)) {
-                    ++i, ++j;
-                } else {
-                    bRet = false;
-                    break;
-                }
-            }
+    bool isMatch(string s, string p) {
+        int i = 0, j = 0, iStar = -1, jStar = -1, m = s.size(), n = p.size();
+        while (i < m) {
+            if (j < n && (s[i] == p[j] || p[j] == '?')) {
+                ++i, ++j;//i，j向后瞬移
+            } else if (j < n && p[j] == '*') {//记录如果之后序列匹配不成功时， i和j需要回溯到的位置
+                iStar = i;//记录星号
+                jStar = ++j;//记录星号 并且j移到下一位 准备下个循环s[i]和p[j]的匹配
+            } else if (iStar >= 0) {//发现字符不匹配且没有星号出现 但是istar>0 说明可能是*匹配的字符数量不对 这时回溯
+                i = ++iStar;//i回溯到istar+1 因为上次从s串istar开始对*的尝试匹配已经被证明是不成功的（不然不会落入此分支） 所以需要从istar+1再开始试 同时inc istar 更新回溯位置
+                j = jStar;//j回溯到jstar+1 重新使用p串*后的部分开始对s串istar（这个istar在上一行已经inc过了）位置及之后字符的匹配 
+            } else return false;
         }
-        if (i == nSLen && j == nPLen) {
-            bRet = true;
-        } else {
-            bRet = false;
-        }
-        return bRet;
+        while (j < n && p[j] == '*') ++j;//去除多余星号
+        return j == n;
     }
-    bool isMatch(string s, string p) { return isMatchHelper(s, p, 0, 0); }
 };
+
+// class Solution
+// {
+// public:
+//     bool isMatch(string s, string p)
+//     {
+//         s = " " + s; //防止该案例："" "c*"
+//         p = " " + p;
+//         int m = s.size();
+//         int n = p.size();
+//         bool dp[m + 1][n + 1];
+//         memset(dp, false, (m + 1) * (n + 1));
+//         dp[0][0] = true;
+//         for (int i = 1; i <= m; i++) {
+//             for (int j = 1; j <= n; j++) {
+//                 if (p[j - 1] == s[i - 1] || p[j - 1] == '?') {
+//                     dp[i][j] = dp[i - 1][j - 1];
+//                 } else if (p[j - 1] == '*') {
+//                     dp[i][j] = dp[i][j - 1] || dp[i - 1][j];
+//                 }
+//             }
+//         }
+//         return dp[m][n];
+//     }
+// };
 
 TEST(leetcode, isMatch)
 {
     Solution s;
-    EXPECT_FALSE(s.isMatch("ab", ".*c"));
+    EXPECT_FALSE(s.isMatch("aab", "c*a*b"));
     EXPECT_FALSE(s.isMatch("aa", "a"));
-    EXPECT_TRUE(s.isMatch("aa", "a*"));
-    EXPECT_TRUE(s.isMatch("aaa", "a*a"));
-    EXPECT_TRUE(s.isMatch("ab", ".*"));
-    EXPECT_TRUE(s.isMatch("aab", "c*a*b"));
-    EXPECT_FALSE(s.isMatch("mississippi", "mis*is*p*."));
+    EXPECT_TRUE(s.isMatch("aa", "*"));
+    EXPECT_FALSE(s.isMatch("cb", "?a"));
+    EXPECT_TRUE(s.isMatch("adceb", "*a*b"));
+    EXPECT_FALSE(s.isMatch("acdcb", "a*c?b"));
 }
 
 int main(int argc, char *argv[])
