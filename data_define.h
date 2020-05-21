10be6a3691d1a2577122ac276a0728e0ab537514
翟宇 <zhaiyu@qianxin.com>
2020-05-21 14:17:00 +0800

    :sparkles: feat(master): 完成公共函数
    Signed-off-by: 翟宇 <zhaiyu@qianxin.com>

--- data_define.h
+++ data_define.h
@@ -17,6 +17,7 @@
 #include <unordered_map>
 #include <unordered_set>
 #include <vector>
+#include <gmock/gmock.h>
 #include <gtest/gtest.h>
 
 using namespace std;
@@ -42,6 +43,85 @@ void print(const vector<vector<T>> &t)
     for (auto it = t.begin(); it != t.end(); ++it) {
         print(*it);
     }
+}
+
+template <typename T>
+std::string getString(const T &t)
+{
+    return std::to_string(t);
+}
+
+template <typename T>
+std::string getString(const vector<T> &t)
+{
+    std::string strRet;
+    for (auto it = t.begin(); it != t.end(); ++it) {
+        if (strRet.empty()) {
+            strRet.append("[");
+            strRet.append(std::to_string(*it));
+        } else {
+            strRet.append(",");
+            strRet.append(std::to_string(*it));
+        }
+    }
+    strRet.append("]");
+    return strRet;
+}
+
+template <typename T>
+std::string getString(const vector<vector<T>> &t)
+{
+    std::string strRet;
+    for (auto it = t.begin(); it != t.end(); ++it) {
+        if (strRet.empty()) {
+            strRet.append("[\n  ");
+            strRet.append(getString(*it));
+        } else {
+            strRet.append(",\n  ");
+            strRet.append(getString(*it));
+        }
+    }
+    strRet.append("\n]");
+    return strRet;
+}
+
+template <typename T>
+bool isEqualWithoutOrder(const vector<T> &t1, const vector<T> &t2)
+{
+    if (t1.size() != t2.size())
+        return false;
+    unordered_set<T> setTmp;
+    for (int i = 0; i < (int)t1.size(); ++i) {
+        setTmp.insert(t1.at(i));
+    }
+    bool bRet = true;
+    for (int i = 0; i < (int)t2.size(); ++i) {
+        if (setTmp.find(t2.at(i)) == setTmp.end()) {
+            bRet = false;
+            break;
+        }
+    }
+    return bRet;
+}
+
+template <typename T>
+bool isEqualWithoutOrder(const vector<vector<T>> &t1,
+                         const vector<vector<T>> &t2)
+{
+    if (t1.size() != t2.size())
+        return false;
+    bool bRet = true;
+    for (int i = 0; i < (int)t2.size() && bRet; ++i) {
+        if (find(t1.begin(), t1.end(), t2.at(i)) == t1.end()) {
+            bRet = false;
+        }
+    }
+    for (int i = 0; i < (int)t1.size() && bRet; ++i) {
+        if (find(t2.begin(), t2.end(), t1.at(i)) == t2.end()) {
+            bRet = false;
+        }
+    }
+    return bRet;
 }
 
 static int n = []() {
