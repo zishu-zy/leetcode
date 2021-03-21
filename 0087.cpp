eb15f278a4aac1987b36d54595760b4499eedf02
zishu <zishuzy@qq.com>
2021-03-21 20:27:18 +0800

    :sparkles: feat(master): 完成87
    Signed-off-by: zishu <zishuzy@qq.com>

--- 0087.cpp
+++ 0087.cpp
@@ -6,37 +6,32 @@ class Solution
 public:
     bool isScramble(string s1, string s2)
     {
-        std::cout << "s1: " << s1 << " s2:" << s2 << std::endl;
-
-        // if (s1.size() != s2.size()) {
-        //     return false;
-        // }
-
         if (s1 == s2) {
             return true;
         }
+        auto it = m_mapTmp.find(s1 + s2);
+        if (it != m_mapTmp.end()) {
+            return it->second;
+        }
+        bool bRet = false;
         for (size_t i = 1; i < s1.size(); i++) {
-            // std::cout << "s1: " << s1.substr(0, i) << " s2:" << s2.substr(0, i) << std::endl;
-            // std::cout << "s1: " << s1.substr(i) << " s2:" << s2.substr(i) << std::endl;
-            // std::cout << "s1: " << s1.substr(0, i) << " s2:" << s2.substr(s2.size() - i)
-            //           << std::endl;
-            // std::cout << "s1: " << s1.substr(i) << " s2:" << s2.substr(0, s2.size() - i)
-            //           << std::endl;
-            // std::cout << "-------" << std::endl;
             if (isScramble(s1.substr(0, i), s2.substr(0, i)) &&
                 isScramble(s1.substr(i), s2.substr(i))) {
-                return true;
+                bRet = true;
+                break;
             }
             if (isScramble(s1.substr(0, i), s2.substr(s2.size() - i)) &&
                 isScramble(s1.substr(i), s2.substr(0, s2.size() - i))) {
-                return true;
+                bRet = true;
+                break;
             }
         }
-        return false;
+        m_mapTmp[s1 + s2] = bRet;
+        return bRet;
     }
 
 private:
-    std::vector<std::string> m_vecRetTmp;
+    std::map<std::string, bool> m_mapTmp;
 };
 
 TEST(Solution, leetcode)
@@ -49,7 +44,7 @@ TEST(Solution, leetcode)
     input_1 = "abcdefghijklmnopq";
     input_2 = "efghijklmnopqcadb";
     output = Solution().isScramble(input_1, input_2);
-    expected = true;
+    expected = false;
     EXPECT_TRUE(output == expected)
         << "\ninput:   " << getString(input_1) << "\n         " << getString(input_2)
         << "\nouput:   " << getString(output) << "\nexpected:" << getString(expected);
